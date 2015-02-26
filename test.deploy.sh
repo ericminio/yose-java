e6f1ea120a549eb5e4942e36078e6e787e1b0e7b
ericminio <eric.mignot@gmail.com>
2015-02-26 14:24:11 -0500

    Travis runs test.deploy with sh -e

--- test.deploy.sh
+++ test.deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 if [ -n "`curl -s https://ericminio-yose-java.herokuapp.com/any`" ]
 then
-echo "App seems offline :("
+echo "App online :)"
 exit 0
 else
-echo "App online :)"
+echo "App seems offline :("
 exit 1
 fi
 
